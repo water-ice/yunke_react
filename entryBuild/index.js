2dafdddeca788e05e1a96f12d96c98f5639625e9
liangchangqing <lcq0202@163.com>
2020-03-29 22:38:23 +0800

    webpack react

---
+++ entryBuild/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import Index from '../app/component/index/Index.jsx';
+
+ReactDOM.render(,document.getElementById('app'));
---
+++ src/index/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Indexextends React.Component {
+    render() {
+        return (
+            这是首页
+        );
+    }
+}
+export default Index;

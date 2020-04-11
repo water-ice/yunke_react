562f508667d9fcd362e882af62985a5ead2a95df
liangchangqing <lcq0202@163.com>
2020-04-11 21:38:58 +0800

    多页面生成build

--- config/webpack/webpack.dev.conf.js
+++ config/webpack/webpack.dev.conf.js
@@ -68,10 +68,10 @@ let config = merge(baseWebpackConfig, {
                       loader: "style-loader" // 将 JS 字符串生成为 style 节点
                     },
                     {
-                      loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+                      loader: "sass-loader" // 将 Sass 编译成 CSS
                     },
                     {
-                      loader: "sass-loader" // 将 Sass 编译成 CSS
+                      loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
                     }
                 ],
                 exclude: [
